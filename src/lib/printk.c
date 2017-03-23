5dc1690f1043610b1fe7c6ed00960a4a6fa4cb00
rainorangelemon <rainorangelemon@gmail.com>
2017-03-23 09:26:00 -0400

    Well, I finish the printk, Now I am going to create the game code.

--- src/lib/printk.c
+++ src/lib/printk.c
@@ -1,12 +1,73 @@
 #include "common.h"
 
+void Extract(char *substr,int p,int orig){
+	uint32_t rest;
+	rest=(uint32_t)orig;
+	char rev[50];
+	char *current=rev;
+	if(rest==0){
+		*(substr+1)='\0';
+		*substr='0';
+		return;
+	}
+	while(rest!=0){
+		int remin=rest%p;
+		if(remin<10){
+			*current='0'+remin;
+		}else{
+			*current='a'+remin-10;
+		}
+		rest=rest/p;
+		current++;
+	}
+	current--;
+	while(current!=rev-1){
+		*substr=*current;
+		substr++;
+		current--;
+	}
+	*substr='\0';
+}
+
 /* implement this function to support printk */
 void vfprintf(void (*printer)(char), const char *ctl, void **args) {
-	const char *str = __FUNCTION__;
-	for(;*str != '\0'; str ++) printer(*str);
-
-	str = ": vfprintf() is not implemented!\n";
-	for(;*str != '\0'; str ++) printer(*str);
+	char c;
+	for(c=*ctl;*ctl!='\0';ctl++,c=*ctl){
+		if(c=='%'&&*(ctl+1)!='\0'){
+			ctl++;
+			char substr[50];
+			if(*ctl=='x'){
+				Extract(substr,16,*(int*)args);
+				char *m;
+				for(m=substr;*m!='\0';m++){
+					printer(*m);
+				}
+				args++;
+			}else if(*ctl=='d'){
+				Extract(substr,10,*(int*)args);
+				char *m;
+				for(m=substr;*m!='\0';m++){
+					printer(*m);
+				}
+				args++;
+			}else if(*ctl=='s'){
+				char *m=*(char **)args;
+				while(*m!='\0'){
+					printer(*m);
+					m++;
+				}
+				args++;
+			}else if(*ctl=='c'){
+				printer(*(char*)args);
+				args++;
+			}else{
+				printer('%');
+				args++;
+			}
+		}else{
+			printer(c);
+		}
+	}
 }
 
 extern void serial_printc(char);
